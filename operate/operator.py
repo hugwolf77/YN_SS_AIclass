3da1961a75d27f8d74475c94f2be8b75a450b39a
hugwolf77 <hugwolf77@gmail.com>
2024-04-02 17:45:47 +0900

    update_02

--- operate/operator.py
+++ operate/operator.py
@@ -1,8 +1,62 @@
 import torch
 from torch import nn
 from torch import optim
-
 from sklearn.metrics import roc_auc_score
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 모델 학습 함수
+def training(model, train_dataloader, train_dataset, optimizer, criterion):
+    print('Training')
+    model.train()
+    train_running_loss = 0.0
+    train_running_correct = 0
+    counter = 0
+    total = 0
+    prog_bar = tqdm(enumerate(train_dataloader), total=int(len(train_dataset)/train_dataloader.batch_size))
+    for i, data in prog_bar:
+        counter += 1
+        data, target = data[0].to(device), data[1].to(device)
+        total += target.size(0)
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, target)
+        train_running_loss += loss.item()
+        _, preds = torch.max(outputs.data, 1)
+        train_running_correct += (preds == target).sum().item()
+        loss.backward()
+        optimizer.step()
+
+    train_loss = train_running_loss / counter
+    train_accuracy = 100. * train_running_correct / total
+    return train_loss, train_accuracy
+
+# 콜백을 적용할 검증 함수
+def validate(model, test_dataloader, val_dataset, criterion):
+    print('Validating')
+    model.eval()
+    val_running_loss = 0.0
+    val_running_correct = 0
+    counter = 0
+    total = 0
+    prog_bar = tqdm(enumerate(test_dataloader), total=int(len(val_dataset)/test_dataloader.batch_size))
+    with torch.no_grad():
+        for i, data in prog_bar:
+            counter += 1
+            data, target = data[0].to(device), data[1].to(device)
+            total += target.size(0)
+            outputs = model(data)
+            loss = criterion(outputs, target)
+
+            val_running_loss += loss.item()
+            _, preds = torch.max(outputs.data, 1)
+            val_running_correct += (preds == target).sum().item()
+
+        val_loss = val_running_loss / counter
+        val_accuracy = 100. * val_running_correct / total
+        return val_loss, val_accuracy
+
 
 ### VALIDATION FUNCTION
 def validation(model, loader, criterion, device="cpu"):
